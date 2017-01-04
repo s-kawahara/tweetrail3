256dfb5ea55dae3978bf1597d8307ad287678d51
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2017-01-04 12:41:04 +0900

    URLの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout"}
+
+  root to: "home#index"
+  # ログイン画面をホームにする（エラーが発生するのでいったんコメントアウト）
+  #devise_scope :user do
+  #  root :to => "devise/sessions#new"
+  #end
   get 'home/index'
 
   get 'home/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+
+
 end
