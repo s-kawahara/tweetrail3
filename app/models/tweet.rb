f65b7737ab9cfddac3bac9b8106251496521e006
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2017-01-04 18:20:49 +0900

    ikeコントローラーの作成とユーザー・投稿との結びつけ

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-
+  has_many :likes
+  validates :content,presence:true, length: {maximum:140}
+  validates :user_id, presence:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :notes
+  has_many :tweets, dependent: :destroy
+  has_many :likes
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
