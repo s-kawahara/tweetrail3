0b164a2f73d5f681d0ce72df58ebe4015172999f
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2017-01-06 17:17:55 +0900

    入力フォームのviewの調整

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TweetsController, type: :controller do
+  render_views
+
   describe '#index'
   it "リクエスト成功" do
     expect(response).to be_success
   end
+
 end
