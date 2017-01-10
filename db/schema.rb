9a582e85b51b694a9cb4d8ff6df337010fd721e5
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2017-01-10 14:24:35 +0900

    usersモデルにimageカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106044810) do
+ActiveRecord::Schema.define(version: 20170110051012) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170106044810) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "image"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
