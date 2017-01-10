4018d6b2a4d8bdd80aa7db8a469ed01fb3698e11
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2017-01-10 20:28:57 +0900

    viewの調整(画像登録機能未実装)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -11,6 +11,7 @@ class RelationshipsController < ApplicationController
   def destroy
     user_id = params[:user_id]
     if Relationship.find_by(following_id: current_user.id, follower_id: user_id).destroy
+		  redirect_to user_path(user_id), notice: "フォロー解除しました"
 	  end
   end
 end
